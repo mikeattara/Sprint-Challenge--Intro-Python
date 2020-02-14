ba90e72c01717497821dcea51156b9d8275e7102
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-02-14 08:27:00 +0000

    print names that start with any letter between C and G

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -40,7 +40,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [human.name for human in humans if human.name[0] in ['C', 'D', 'E', 'F', 'G']]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
